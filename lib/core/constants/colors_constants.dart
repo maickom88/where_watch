f4de7e4ec86cd9385beb63ad2c7126b043ff6f7e
Michael Frank <maickomtutos@gmail.com>
2021-06-11 09:44:29 -0300

    changed fonts ad finality theme app

--- lib/core/constants/colors_constants.dart
+++ lib/core/constants/colors_constants.dart
@@ -4,5 +4,6 @@ class AppColors {
   static const Color primaryColor = Color(0xFF8342D6);
   static const Color secondaryColor = Color(0xFF973CD3);
   static const Color accentColor = Color(0xff332E3A);
+  static const Color textColor = Color(0xffABABAB);
   static const Color backgroundColor = Color(0xff2A272E);
 }
